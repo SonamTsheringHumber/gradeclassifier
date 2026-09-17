811ea012be5e0b6447294678a1d39980efe7a454
Sonam Tshering <sonamtshering1213@gmail.com>
2026-09-17 10:43:26 -0400

    update code indentation

--- gradeclassifier.py
+++ gradeclassifier.py
@@ -1,20 +1,22 @@
-mark=input("Enter your mark:")
+mark = input("Enter your mark: ")
+
 try:
-    mark=float(mark)
-    if (mark<0) or (mark>100):
-        print("Invalied mark. Enter a mark between 0 and 100")
-    elif (mark>=90):
+    mark = float(mark)
+
+    if mark < 0 or mark > 100:
+        print("Invalid mark. Enter a mark between 0 and 100.")
+    elif mark >= 90:
         print(f"Your mark is {mark}: Grade A")
-    elif (mark>=80):
+    elif mark >= 80:
         print(f"Your mark is {mark}: Grade B")
-    elif (mark>=70):
+    elif mark >= 70:
         print(f"Your mark is {mark}: Grade C")
-    elif (mark>=60):
+    elif mark >= 60:
         print(f"Your mark is {mark}: Grade D")
-    elif (mark>=50):
+    elif mark >= 50:
         print(f"Your mark is {mark}: Grade E")
     else:
         print(f"Your mark is {mark}: Grade F (Fail)")
+
 except ValueError:
-    print("invalid input. Enter a mark in number between 0 and 100")
-        
+    print("Invalid input. Enter a number between 0 and 100.")
