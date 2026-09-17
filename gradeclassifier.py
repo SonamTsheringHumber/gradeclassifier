b796c80127f5948894b8e49f0128c824a37decd1
Sonam Tshering <sonamtshering1213@gmail.com>
2026-09-17 10:11:21 -0400

    improve output display

--- gradeclassifier.py
+++ gradeclassifier.py
@@ -1,17 +1,14 @@
-
 mark= input("Enter your mark: ")
-
 try:
     mark = float(mark)
-
     if mark < 0 or mark > 100:
-        print("Invalid mark. Please enter a value between 0 and 100.")
+        print("Invalid mark. Please enter a mark between 0 and 100.")
     elif mark >= 80:
-        print("Grade A.")
+        print(f"Your mark is {mark}: Grade A")
     elif mark >= 60:
-        print("Grade B.")
+        print(f"Your mark is {mark}: Grade B")
     else:
-        print("Grade C, Need improvement")
+        print(f"Your mark is {mark}: Grade C")
 
 except ValueError:
     print("Invalid input. Please enter a mark in number.")
